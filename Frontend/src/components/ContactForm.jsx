21f35194eac65a8d301fbe2116ce8755fef4e65d
MSOR03 <molartera@unal.edu.co>
2025-06-01 00:21:07 -0500

    Fixed package.json

--- Frontend/src/components/ContactForm.jsx
+++ Frontend/src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+{/*hOLA MUNDO*/}
 import { Button } from "@/components/ui/button";
 
 const ContactForm = () => {
--- Frontend/src/components/Technologies.jsx
+++ Frontend/src/components/Technologies.jsx
@@ -60,7 +60,7 @@ const Technologies = () => {
             ))}
           </div>
         </div>
-
+        {/* Prueba */}
         {/* Desarrollo web */}
         <div>
           <h3 className="text-2xl font-semibold text-white mb-6">Programación</h3>
